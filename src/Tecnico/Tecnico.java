219cf5eae731c15b2d716fb8558709f79cf15fdf
Ronny Sandino Alvarado <rosandinoal@est.utn.ac.cr>
2023-11-26 17:27:17 -0600

    Clase Tecnico
    se programa la clase tecnico

--- src/Tecnico/Tecnico.java
+++ src/Tecnico/Tecnico.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Tecnico;
 
 /**
@@ -9,5 +5,110 @@ package Tecnico;
  * @author ´Felipe Chacón
  */
 public class Tecnico {
-    
+
+    private String cedula;
+    private String nombre;
+    private String fechaNacimiento;
+    private String telefono;
+    private String correo;
+    private double salario;
+    private String password;
+
+    public Tecnico(String cedula, String nombre, String fechaNacimiento, String telefono, String correo, double salario) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.salario = salario;
+        this.password = "";
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void calcularDeducciones() {
+        double enfermedadMaternidad = salario * 0.055;
+        double invalidezMuerte = salario * 0.0384;
+        double aporteTrabajador = salario * 0.01;
+        double aporteSolidarista = salario * 0.033;
+
+        // Impuesto sobre la renta
+        double exento = 817000;
+        double impuestoRenta = 0;
+
+        if (salario > exento && salario <= 1226000) {
+            impuestoRenta = (salario - exento) * 0.1;
+        } else if (salario > 1226000) {
+            impuestoRenta = (salario - 1226000) * 0.15 + (1226000 - exento) * 0.1;
+        }
+        // Restar impuesto después del cálculo
+        double salarioNeto = salario - enfermedadMaternidad - invalidezMuerte - aporteTrabajador - aporteSolidarista - impuestoRenta;
+    }
+
+    public void cambiarContrasena(String contrasenaActual, String nuevaContrasena) {
+        if (validarContrasena(contrasenaActual)) {
+            this.password = nuevaContrasena;
+            System.out.println("Contraseña cambiada exitosamente.");
+        } else {
+            System.out.println("Error: Contraseña actual incorrecta.");
+        }
+    }
+
+    public boolean validarContrasena(String contrasena) {
+        return this.password.equals(contrasena);
+    }
 }
