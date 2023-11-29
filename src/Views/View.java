6f50c6bbda72a34e84234ed7ac96233cdd73f534
Ronny Sandino Alvarado <rosandinoal@est.utn.ac.cr>
2023-11-28 22:52:58 -0600

    Interface View
    se agrega la interface view

---
+++ src/Views/View.java
@@ -0,0 +1,27 @@
+package Views;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
+public interface View<Class> {
+
+    public static void showInternal(JDesktopPane desk, JInternalFrame frm) {
+        int desktopWidth = desk.getWidth();
+        int desktopHeight = desk.getHeight();
+        int internalFrameWidth = frm.getWidth();
+        int internalFrameHeight = frm.getHeight();
+        int x = (desktopWidth - internalFrameWidth) / 2;
+        int y = (desktopHeight - internalFrameHeight) / 2;
+        frm.setLocation(x, y);
+        desk.add(frm);
+        frm.setVisible(true);
+    }
+        void displayMessage(String message);
+
+    public void display(Class obj);
+    
+    public static void maximize(JFrame frm) {
+        frm.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+}
